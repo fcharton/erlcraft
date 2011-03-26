ae5efb07c89df68ad9b6d79a527ab78f7adfb660
Florent Charton <florent.charton@free.fr>
2011-03-26 17:28:06 +0100

    Is now properly loading the Beta format files

--- src/mc_region.erl
+++ src/mc_region.erl
@@ -5,8 +5,8 @@
 -define(LOC_OFFSET,  24/unsigned-big-integer).
 -define(LOC_COUNT,    8/unsigned-big-integer).
 -define(TIMESTAMP,   32/unsigned-big-integer).
--define(DATA_LENGTH, 32/unsigned-big-integer).
--define(DATA_CPTYPE,  8/unsigned-big-integer).
+-define(DATA_LENGTH, 32/signed-big-integer).
+-define(DATA_CPTYPE,  8/signed-big-integer).
 
 load_file(Path) ->
     case file:read_file(Path) of
@@ -20,16 +20,16 @@ load_file(Path) ->
 
 locations_to_list(<<>>, _Timestamps) ->
     [];
-locations_to_list(<< 
-		     0:?LOC_OFFSET, 
-		     0:?LOC_COUNT, 
-		     Rest/binary 
-		  >>, 
-		  <<
-		    Timestamp:?TIMESTAMP, 
-		    Timestamps/binary
-		  >>) ->
-    locations_to_list(Rest, Timestamps);
+%% locations_to_list(<< 
+%% 		     0:?LOC_OFFSET, 
+%% 		     0:?LOC_COUNT, 
+%% 		     Rest/binary 
+%% 		  >>, 
+%% 		  <<
+%% 		    Timestamp:?TIMESTAMP, 
+%% 		    Timestamps/binary
+%% 		  >>) ->
+%%     locations_to_list(Rest, Timestamps);
 locations_to_list(<<
 		    Offset:?LOC_OFFSET,
 		    Count:?LOC_COUNT,
@@ -41,16 +41,35 @@ locations_to_list(<<
 		  >>) ->
     [{Offset, Count, Timestamp}|locations_to_list(Rest, Timestamps)].
 
-load_chunks([], CData) ->
+load_chunks([], _CData) ->
     {ok};
-load_chunks([{COffset, CSize, CTime}|CList], Chunks) ->
-    Offset = COffset * 4096,
+load_chunks([{0,0,_CTime}|CList], CData) ->
+    load_chunks(CList, CData);
+load_chunks([{COffset, CSize, CTime}|CList], CData) ->
+    io:format("Chunk Input : ~p ~p~n", [COffset, CSize]),
+    % CData is the binary after the header
+    Offset = COffset * 4096 - 8192,
+    DataSize = CSize * 512,
+    io:format("Chunk Seek : ~p ~p~n", [Offset+8192, DataSize*8]),
     <<
       _:Offset/binary,
       CLength:?DATA_LENGTH,
       CType:?DATA_CPTYPE,
-      Data/binary
-    >> = Chunks,
-    io:format("Chunk : ~p ~p~n", [CLength, CType]),
-    load_chunks(CList, Chunks).
+      ZippedData:DataSize/binary,
+      _/binary
+    >> = CData,
+    {ok, RawData} = case CType of
+	2 -> zlib_read_chunk(ZippedData);
+	_ -> {error, enoimpl}
+    end,
+    NBTData = nbt:parse_nbt(RawData),
+    io:format("~p~n", [NBTData]).
+%    load_chunks(CList, CData).
 
+zlib_read_chunk(ZData) ->
+    Z = zlib:open(),
+    zlib:inflateInit(Z),
+    Data = zlib:inflate(Z, ZData),
+    zlib:close(Z),
+    io:format("~p~n", [list_to_binary(Data)]),
+    {ok, list_to_binary(Data)}.
